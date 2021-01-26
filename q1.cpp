6067156d22fab8d6e41506f60b45c75d889823da
gupta31 <sanchigupta123456@gmail.com>
2021-01-26 23:41:36 +0530

    init

--- q1.cpp
+++ q1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
   public:
     void prank(long long a[], int n){
-        // code here//y=a[i],x=a[a[i]];(x*n+y) or (x%n)*n+y
+        // y=a[i],x=a[a[i]];(x*n+y) or (x%n)*n+y
         for(int i=0;i<n;i++){
             a[i]=a[i]+(a[a[i]]%n)*n;
         }
